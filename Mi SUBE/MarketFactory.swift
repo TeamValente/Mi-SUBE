c8579d85c7e64cfb9c7b037f28546bb07e139ab0
Davo <davidgalavotti@gmail.com>
2015-12-06 16:36:20 -0300

    Agrego mk_selected

--- Mi SUBE/MarketFactory.swift
+++ Mi SUBE/MarketFactory.swift
@@ -45,7 +45,7 @@ class MarketFactory{
         puntoRetorno.title = miPunto.address
         puntoRetorno.subtitle = miPunto.detalleParaMapa()
         //point.index = miPunto.idPunto
-        puntoRetorno.imageSelected = "mk_fav"
+        puntoRetorno.imageSelected = "mk_selected"
         puntoRetorno.coordinate = CLLocationCoordinate2D(latitude: miPunto.latitude, longitude: miPunto.longitude)
     
         return puntoRetorno
