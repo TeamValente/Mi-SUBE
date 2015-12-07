c7d3e6a9751db17d008b6bd09c5468b61cb1d512
kashwaksoft <hernan.coppola@gmail.com>
2015-12-07 01:23:45 -0300

    Performance en connexion.

--- Mi SUBE/DondeCargoService.swift
+++ Mi SUBE/DondeCargoService.swift
@@ -26,63 +26,68 @@ class DondeCargoService{
                     request.HTTPBody = bodyData.dataUsingEncoding(NSUTF8StringEncoding);
                 }
                 request.HTTPMethod = "POST"
-                let task = NSURLSession.sharedSession().dataTaskWithRequest(request){ (data, response, error) in
+                let task = NSURLSession.sharedSession().dataTaskWithRequest(request){ (data1, response, error) in
                     if error != nil {
                         //print("error=\(error)")
                         callback(nil)
                     }
                     
-                    if data == nil{
-                        callback(nil)
-                    }
-                    
-                    var listadoPuntos = [PuntoCarga]()
-                    var horaApertura = 0
-                    var horaCierre = 0
-                    var cost = 0
-                    //Parseo el data
-                    do{
-                        let jsonDictionary = try NSJSONSerialization.JSONObjectWithData(data!, options: NSJSONReadingOptions.AllowFragments) as! NSArray
-                        //print(jsonDictionary.count)
-                        for index in 0...jsonDictionary.count-1{
-                            let item : AnyObject? = jsonDictionary[index]
-                            let punto = item as! Dictionary<String, AnyObject>
-                            if let puntoId = punto["id"] as? String{
-                                if let direccion = punto["address"] as? String{
-                                    if let latitud = punto["latitude"] as? String{
-                                        if let longitud = punto["longitude"] as? String{
-                                            if let tipo = punto["type"] as? String{
-                                                if let icono = punto["icon"] as? String{
-                                                    if let hourOpen = punto["hopen"] as? String{
-                                                        horaApertura = Int(hourOpen)!
+                    if let data = data1{
+                        var listadoPuntos = [PuntoCarga]()
+                        var horaApertura = 0
+                        var horaCierre = 0
+                        var cost = 0
+                        //Parseo el data
+                        do{
+                            let jsonDictionary = try NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.AllowFragments) as! NSArray
+                            //print(jsonDictionary.count)
+                            for index in 0...jsonDictionary.count-1{
+                                let item : AnyObject? = jsonDictionary[index]
+                                let punto = item as! Dictionary<String, AnyObject>
+                                if let puntoId = punto["id"] as? String{
+                                    if let direccion = punto["address"] as? String{
+                                        if let latitud = punto["latitude"] as? String{
+                                            if let longitud = punto["longitude"] as? String{
+                                                if let tipo = punto["type"] as? String{
+                                                    if let icono = punto["icon"] as? String{
+                                                        if let hourOpen = punto["hopen"] as? String{
+                                                            horaApertura = Int(hourOpen)!
+                                                        }
+                                                        if let hourClose = punto["hclose"] as? String{
+                                                            horaCierre = Int(hourClose)!
+                                                        }
+                                                        if let costoCarga = punto["cost"] as? String{
+                                                            cost = Int(costoCarga)!
+                                                        }
+                                                        //Cargo el punto
+                                                        
+                                                        listadoPuntos.append(PuntoCarga(idPunto: Int(puntoId)!, address: direccion.htmlDecoded(), latitude: Double(latitud)!, longitude: Double(longitud)!, type: tipo, icon: icono,cost: cost, hourOpen: horaApertura, hourClose: horaCierre))
                                                     }
-                                                    if let hourClose = punto["hclose"] as? String{
-                                                        horaCierre = Int(hourClose)!
-                                                    }
-                                                    if let costoCarga = punto["cost"] as? String{
-                                                        cost = Int(costoCarga)!
-                                                    }
-                                                    //Cargo el punto
-                                                    
-                                                    listadoPuntos.append(PuntoCarga(idPunto: Int(puntoId)!, address: direccion.htmlDecoded(), latitude: Double(latitud)!, longitude: Double(longitud)!, type: tipo, icon: icono,cost: cost, hourOpen: horaApertura, hourClose: horaCierre))
                                                 }
                                             }
                                         }
                                     }
                                 }
                             }
+                            dispatch_async(dispatch_get_main_queue()){
+                                callback(listadoPuntos)
+                            }
+                            
+                        }catch{
+                            //No logro parsear el JSON
+                            dispatch_async(dispatch_get_main_queue()){
+                                callback(nil)
+                            }
                         }
-                        dispatch_async(dispatch_get_main_queue()){
-                            callback(listadoPuntos)
-                        }
-                        
-                    }catch{
-                        //No logro parsear el JSON
+                    }else{
                         dispatch_async(dispatch_get_main_queue()){
                             callback(nil)
                         }
                     }
                 }
+
+                        
+                    
                 task.resume()
             }
         }
