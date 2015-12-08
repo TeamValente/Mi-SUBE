8951e02015bf45e47b0d33fb72ef5ef0ee44c4fa
Mariano Molina <xsin.crueldadx@gmail.com>
2015-12-08 15:01:51 -0300

    Implementación de los iconos para el tab bar

--- Mi SUBE/TabBarController.swift
+++ Mi SUBE/TabBarController.swift
@@ -9,10 +9,35 @@
 import UIKit
 import UIColor_Hex_Swift
 
-class TabBarController: UITabBarController {
+class TabBarController: UITabBarController, UINavigationControllerDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // Sets the default color of the icon of the selected UITabBarItem and Title
+        UITabBar.appearance().tintColor = UIColor.whiteColor()
+
+        // Sets the default color of the background of the UITabBar
         UITabBar.appearance().barTintColor = UIColor(rgba: "#207BF6")
+        
+        // Sets the background color of the selected UITabBarItem (using and plain colored UIImage with the width = 1/5 of the tabBar (if you have 5 items) and the height of the tabBar)
+        UITabBar.appearance().selectionIndicatorImage = UIImage().makeImageWithColorAndSize(UIColor(rgba: "#236ED6"), size: CGSizeMake(tabBar.frame.width/3, tabBar.frame.height))
+        
+        // Uses the original colors for your images, so they aren't not rendered as grey automatically.
+        for item in (self.tabBar.items as [UITabBarItem]!)! {
+            if let image = item.image {
+                item.image = image.imageWithRenderingMode(.AlwaysOriginal)
+            }
+        }
     }
-    
+}
+
+extension UIImage {
+    func makeImageWithColorAndSize(color: UIColor, size: CGSize) -> UIImage {
+        UIGraphicsBeginImageContextWithOptions(size, false, 0)
+        color.setFill()
+        UIRectFill(CGRectMake(0, 0, size.width, size.height))
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return image
+    }
 }
