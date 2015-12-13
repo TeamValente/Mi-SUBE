491954e35e3edb79b16b1a0ae89da73cd2cdd47d
kashwaksoft <hernan.coppola@gmail.com>
2015-12-12 21:04:49 -0300

    Vista Route

---
+++ Mi SUBE/RouteViewController.swift
@@ -0,0 +1,38 @@
+//
+//  RouteViewController.swift
+//  Mi SUBE
+//
+//  Created by Hernan Matias Coppola on 12/12/15.
+//  Copyright © 2015 Hernan Matias Coppola. All rights reserved.
+//
+
+import UIKit
+
+class RouteViewController: UIViewController {
+
+    
+    var puntoDestino:PuntoCarga!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
