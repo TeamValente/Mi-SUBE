fb933418d4e4226db961da745306ee0542acf0cd
Mariano Molina <xsin.crueldadx@gmail.com>
2015-12-18 00:05:39 -0300

    Implementación de la vista configuración

--- Mi SUBE/ConfigTableViewController.swift
+++ Mi SUBE/ConfigTableViewController.swift
@@ -29,7 +29,7 @@ class ConfigTableViewController: UITableViewController {
 
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 3
+        return 4
     }
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
