a2d9c8d36e95d9a6564dfd9363db50694ac29f60
Mariano Molina <xsin.crueldadx@gmail.com>
2016-01-23 19:44:54 -0300

    :rocket: Change OssView to webview

--- Mi SUBE/AboutViewController.swift
+++ Mi SUBE/AboutViewController.swift
@@ -29,7 +29,6 @@ class AboutViewController: UIViewController, UIWebViewDelegate {
     }
     
     func webView(webView: UIWebView, shouldStartLoadWithRequest request: NSURLRequest, navigationType: UIWebViewNavigationType) -> Bool {
-        
         let url: NSURL = request.URL!
         let isExternalLink: Bool = url.scheme == "http" || url.scheme == "https" || url.scheme == "mailto"
         if (isExternalLink && navigationType == UIWebViewNavigationType.LinkClicked) {
--- Mi SUBE/OssViewController.swift
+++ Mi SUBE/OssViewController.swift
@@ -8,48 +8,32 @@
 
 import UIKit
 
-class OssViewController: UIViewController {
+class OssViewController: UIViewController, UIWebViewDelegate {
 
+    @IBOutlet weak var ossWebView: UIWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         UIApplication.sharedApplication().statusBarStyle = .LightContent
+        
+        ossWebView.delegate = self
+        
+        let url = NSURL(string: "http://misube.com/libs.html")
+        let requestObj = NSURLRequest(URL: url!)
+        ossWebView.loadRequest(requestObj)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
     
-    @IBAction func openLink(sender: UIButton) {
-        switch(sender.tag) {
-            case 1 :
-                if let cocoapodsLink = NSURL(string: "https://cocoapods.org") {
-                    UIApplication.sharedApplication().openURL(cocoapodsLink)
-                }
-            case 2 :
-                if let realmLink = NSURL(string: "https://realm.io") {
-                    UIApplication.sharedApplication().openURL(realmLink)
-                }
-            case 3 :
-                if let uicolorLink = NSURL(string: "https://github.com/yeahdongcn/UIColor-Hex-Swift") {
-                    UIApplication.sharedApplication().openURL(uicolorLink)
-                }
-            case 4 :
-                if let progressLink = NSURL(string: "https://github.com/kentya6/KYCircularProgress") {
-                    UIApplication.sharedApplication().openURL(progressLink)
-                }
-            case 5 :
-                if let springLink = NSURL(string: "https://github.com/MengTo/Spring") {
-                    UIApplication.sharedApplication().openURL(springLink)
-                }
-        case 6 :
-            if let dondeCargoLaSUBE = NSURL(string: "http://dondecargolasube.com.ar/#footer") {
-                UIApplication.sharedApplication().openURL(dondeCargoLaSUBE)
-            }
-            default:
-                print("tag erroneo!")
-            
+    func webView(webView: UIWebView, shouldStartLoadWithRequest request: NSURLRequest, navigationType: UIWebViewNavigationType) -> Bool {
+        let url: NSURL = request.URL!
+        let isExternalLink: Bool = url.scheme == "http" || url.scheme == "https" || url.scheme == "mailto"
+        if (isExternalLink && navigationType == UIWebViewNavigationType.LinkClicked) {
+            return !UIApplication.sharedApplication().openURL(request.URL!)
+        } else {
+            return true
         }
-    
     }
-    
 }
