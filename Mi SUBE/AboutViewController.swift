56cfcc2fb3708cd04748d7744677e5b9fba64772
Mariano Molina <xsin.crueldadx@gmail.com>
2016-01-23 19:18:45 -0300

    :rocket: open link in new safari tab

--- Mi SUBE/AboutViewController.swift
+++ Mi SUBE/AboutViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class AboutViewController: UIViewController {
+class AboutViewController: UIViewController, UIWebViewDelegate {
 
     @IBOutlet weak var aboutWebView: UIWebView!
     
@@ -17,6 +17,8 @@ class AboutViewController: UIViewController {
         
         UIApplication.sharedApplication().statusBarStyle = .LightContent
         
+        aboutWebView.delegate = self
+        
         let url = NSURL(string: "http://misube.com/about.html")
         let requestObj = NSURLRequest(URL: url!)
         aboutWebView.loadRequest(requestObj)
@@ -24,6 +26,17 @@ class AboutViewController: UIViewController {
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+    }
+    
+    func webView(webView: UIWebView, shouldStartLoadWithRequest request: NSURLRequest, navigationType: UIWebViewNavigationType) -> Bool {
+        
+        let url: NSURL = request.URL!
+        let isExternalLink: Bool = url.scheme == "http" || url.scheme == "https" || url.scheme == "mailto"
+        if (isExternalLink && navigationType == UIWebViewNavigationType.LinkClicked) {
+            return !UIApplication.sharedApplication().openURL(request.URL!)
+        } else {
+            return true
+        }
     }
     
 }
