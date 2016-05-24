a275a83a89202e9f09b3692168d1d31cbafdc723
Mariano Molina <xsin.crueldadx@gmail.com>
2016-05-23 23:19:01 -0300

    Remove Mixpanel framework

--- Mi SUBE/AppDelegate.swift
+++ Mi SUBE/AppDelegate.swift
@@ -19,8 +19,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         
         Fabric.with([Crashlytics.self])
-        MixPanelHelper.applicationWasOpened()
-        
         // Override point for customization after application launch.
         return true
     }
@@ -31,28 +29,19 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func applicationDidEnterBackground(application: UIApplication) {
-        
-        MixPanelHelper.applicationEnteredBackground()
-        
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
     }
 
     func applicationWillEnterForeground(application: UIApplication) {
-        
-        MixPanelHelper.applicationEnteredForeground()
         // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
     }
 
     func applicationDidBecomeActive(application: UIApplication) {
-        
-        MixPanelHelper.applicationBecameActive()
-        
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
     }
 
     func applicationWillTerminate(application: UIApplication) {
-        MixPanelHelper.applicationWasClosed()
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
 
--- Podfile
+++ Podfile
@@ -8,7 +8,6 @@ target 'Mi SUBE' do
     pod 'RealmSwift'
     pod 'KYCircularProgress', '~> 0.5'
     pod 'Spring', :git => 'https://github.com/MengTo/Spring.git'
-    pod 'Mixpanel'
     pod 'Fabric'
 	pod 'Crashlytics'
 end
