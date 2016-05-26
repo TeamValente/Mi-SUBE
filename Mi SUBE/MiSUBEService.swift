5e450fb588892555b12befd954aa38a149fd2837
Mariano Molina <xsin.crueldadx@gmail.com>
2016-05-26 20:17:54 -0300

    Add Service

---
+++ Mi SUBE/MiSUBEService.swift
@@ -0,0 +1,18 @@
+//
+//  MiSUBEService.swift
+//  Mi SUBE
+//
+//  Created by Mariano Molina on 26/5/16.
+//  Copyright © 2016 Hernan Matias Coppola. All rights reserved.
+//
+
+import Foundation
+
+class MiSUBEService {
+    
+    var mFiltro: Filtro!
+    
+    
+    
+    
+}
