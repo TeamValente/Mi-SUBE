6696e5fb2237853a967aab3a4cf928c3581ca2f0
Hernan Matias Coppola <hernan.coppola@gmail.com>
2016-05-26 20:44:21 -0300

    Primeros pasos de la migración a Alamo

--- Mi SUBE/MiSUBEService.swift
+++ Mi SUBE/MiSUBEService.swift
@@ -7,12 +7,39 @@
 //
 
 import Foundation
+import Alamofire
 
 class MiSUBEService {
     
     var mFiltro: Filtro!
     
+    func generarURLValida(url: String) -> String {
+        return url.stringByAddingPercentEncodingWithAllowedCharacters(NSCharacterSet.URLQueryAllowedCharacterSet())!
+    }
     
     
+    func obtenerPuntosPOST(dondeEstoy: MiUbicacion?, callback: [PuntoCarga]? ->()){
+        
+        let parameters = [
+            "session": "1390472",
+            "params": [
+                "lat": dondeEstoy?.latitude,
+                "lng": dondeEstoy?.longitude]
+            
+        ]
+
+        Alamofire.request(.POST, self.generarURLValida("http://dondecargolasube.com.ar/core/?query=getNearPoints"), parameters: parameters)
+         .validate()
+         .responseJSON { response in
+             switch response.result {
+             case .Success:
+                 print("Validation Successful")
+             case .Failure(let error):
+                 print(error)
+             }
+         }
+        // HTTP body: foo=bar&baz[]=a&baz[]=1&qux[x]=1&qux[y]=2&qux[z]=3
+
+    }
     
 }
